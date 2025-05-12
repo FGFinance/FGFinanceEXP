63bdb710f65b128ca7bd80d78dbaa4428fc3b61a
Cherlyte <dogchase4@hotmail.com>
2025-05-12 17:37:42 -0300

    light and dark mode changes

--- resc/js/main.js
+++ resc/js/main.js
@@ -67,6 +67,7 @@ function switchMode(){
   const cmode = document.body.className
   const nmode = cmode == "light-mode" ? "dark-mode" : "light-mode"
   document.body.className = nmode
+  document.getElementById("modeswitchb").innerHTML = nmode == "dark-mode" ? "☀️" : "🌙"
 }
 
 updatePage()
