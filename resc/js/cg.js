88df06df799a4d275aa157394026f18654e40c82
Cherlyte <dogchase4@hotmail.com>
2025-06-16 14:33:48 -0300

    fix

--- resc/js/cg.js
+++ resc/js/cg.js
@@ -129,14 +129,14 @@ function startup(){
 }
 
 function getCookie(name) {
-  let name = cname + "=";
+  let dname = name + "=";
   let ca = document.cookie.split(';');
   for(let i = 0; i < ca.length; i++) { let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1)
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length)
+    if (c.indexOf(dname) == 0) {
+      return c.substring(dname.length, c.length)
     }
   }
   return "";
