e0751e8a8b4146842f72ea4dc31e775bfc63fe34
Cherlyte <dogchase4@hotmail.com>
2025-06-16 14:21:45 -0300

    testing

--- resc/js/index.js
+++ resc/js/index.js
@@ -1,6 +1,41 @@
 function switchMode(){
   const cmode = document.body.className
   const nmode = cmode == "light-mode" ? "dark-mode" : "light-mode"
+  setCookie("lmode", nmode)
   document.body.className = nmode
   document.getElementById("modeswitchb").innerHTML = nmode == "dark-mode" ? "☀️" : "🌙"
 }
+
+function Startup(){
+    const darkval = getCookie("lmode")
+    if(darkval != ""){
+        const nmode = cmode == "light-mode" ? "dark-mode" : "light-mode"
+        document.body.className = nmode
+        document.getElementById("modeswitchb").innerHTML = nmode == "dark-mode" ? "☀️" : "🌙"
+    }else{
+        setCookie("lmode", "light-mode")
+    }
+}
+
+function getCookie(name) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) { let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return "";
+}
+
+function setCookie(name, value) {
+  const d = new Date();
+  d.setTime(d.getTime() + (60 * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+Startup()
