58c2ae79bf1fca10b94ee4c8bcb2f785b45cb440
Cherlyte <dogchase4@hotmail.com>
2025-12-01 16:52:22 -0300

    It works, results don't match with real data but may be because they use yearly prediction (as in the monthly rate changes per month instead of being fixed rate as ours is right now). It'll have to do.

--- resc/js/test.js
+++ resc/js/test.js
@@ -1,16 +1,32 @@
 const yearlyrate = 14.9
-const months = 12
-const monthlyrate = (1+yearlyrate)/12-1
-const inv = 5
+const months = 24
+const monthlyrate = yearlyrate/12
+const inv = 12
+const adder = 2
 let final = 0
+let TAXES = 0
+let buildup = inv
+let buildup2 = 0
 const finrate = monthlyrate*months
-let TAXES = 0.96
-for(let i = 0; i < 29; i+=months/30){
-  if(i >= 30) return
-  TAXES-=0.03 
+if(months >= 24){
+  TAXES = 0.15
+}else if(months >= 12){
+  TAXES = 0.175
+}else if(months >= 6){
+  TAXES = 0.2
+}else{
+  TAXES = 0.225
 }
-if(TAXES < 0) TAXES = 0
+
+for(let i = 0; i < months; i++){
+  buildup2 += (buildup*(monthlyrate/100))
+  buildup += adder
+}
+
 final = (inv*(finrate/100))
+let TAXED = final*TAXES
+console.log((inv+final)-TAXED)
 console.log(inv+final)
 console.log(final)
-console.log(TAXES)
+console.log(TAXED)
+console.log(buildup+buildup2)
